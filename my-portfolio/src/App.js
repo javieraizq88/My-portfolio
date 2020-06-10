0ec847adb6bceed3bcec099bc542506b7fed7955
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-10 18:01:38 -0400

    delete navbar component

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import injectContext from "./store/appContext";
 import Home from "./views/home";
+import About from "./components/aboutHome";
 
 const App = props => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
 
             </Switch>
         </BrowserRouter>

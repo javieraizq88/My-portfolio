58ed953ae4afc62c37b0213f0f74edd0cfcd0c6f
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-11 11:53:23 -0400

    bg project

--- my-portfolio/src/components/projectsHome.js
+++ my-portfolio/src/components/projectsHome.js
@@ -42,81 +42,97 @@ const Projects = props => {
             <br />
 
 
-            <div className="row">
+            <div className="row" id="project">
                 <div className="col-md-1"></div>
                 <div className="col-md-10 col-xs-10 mr-2 mt-5 mb-5">
                     <h1>Projects</h1>
                     <hr />
                     <div className="card-group">
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Project 1</h4>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                        <div className="col-md-4 mb-5">
+                            <div className="card h-100">
+                                <h3 className="card-title " id="titulo-card-project">Starwars</h3>
+                                <div className="card-body">
+                                    <img className="card-img-top" src="http://placehold.it/300x200" alt="" />
+                                    <br />
+                                    <br />
+                                    <h5 className="card-text">Tecnology and Lenguages</h5>
+                                    <ul>
+                                        <lo>
+                                            HTML 5 -
+                                            CSS 3 -
+                                            Bootstrap  -
+                                            React.Js  -
+                                            JavaScript -
+                                            API  -
+                                            J Query
+                                        </lo>
+
+                                    </ul>
+
+                                    <p className="card-text">(Use Fire Fox to open this projects please)</p>
                                 </div>
-                                <div class="card-footer">
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Project 2</h4>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                                </div>
-                                <div class="card-footer">
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Project 3</h4>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                                </div>
-                                <div class="card-footer">
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Find Out More!</a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Project 4</h4>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                        <div className="col-md-4 mb-5">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="http://placehold.it/300x200" alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">Project 2</h4>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
                                 </div>
-                                <div class="card-footer">
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Project 5</h4>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                                </div>
-                                <div class="card-footer">
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Find Out More!</a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
-                                <div class="card-body">
-                                    <h4 class="card-title">Project 6</h4>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                        <div className="col-md-4 mb-5">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="http://placehold.it/300x200" alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">Project 3</h4>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
                                 </div>
-                                <div class="card-footer">
-                                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Find Out More!</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 mb-5">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="http://placehold.it/300x200" alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">Project 4</h4>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                                </div>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Find Out More!</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 mb-5">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="http://placehold.it/300x200" alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">Project 5</h4>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                                </div>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Find Out More!</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 mb-5">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="http://placehold.it/300x200" alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">Project 6</h4>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                                </div>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Find Out More!</a>
                                 </div>
                             </div>
                         </div>

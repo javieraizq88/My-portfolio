a5360c5545f505ddba62eae2c6b55636231dcf1d
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-11 13:15:21 -0400

    CSS imagenes home de SW

--- my-portfolio/src/components/starwars/homeStarWars.js
+++ my-portfolio/src/components/starwars/homeStarWars.js
@@ -13,13 +13,13 @@ const HomeStarWars = props => {
         <div id="container-home-starwars">
         <Titulo/>
         <NavbarStarWars />
-            <div className="ml-5" >
-                <img id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLAUg8wPXTabhE2uenCinJc-IkB607f5mzGdOKUpqZH-K1Gtlu"/>
-                <img id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlRo9f3QeZ2HfygAzbgbZHg4BfKNpre_eZuysD9jwXBotldV29"/>
-                <img id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQlC0-VRfVSfArnhoHZGt8iCBE0ash5jqgSB5bhQiS-uCDWNfM"/>
-                <img id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTnv2-5xQrJBjGNOosj6znfqVa38iaBUVfTWa61xj8cT9h7CMhF"/>
-                <img id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKW5PWRfKi2NA6GkidyAqFMh9UVWRTOUZRO3FSXRqWd8lDduZF"/>
-                <img id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVquJ9ClQnn2xnTeIKV6GYfYEE5lZWvknSdBZihycMBx2Ifpma"/>
+            <div className="card-group" >
+                <img className="card-img col-md-4 col-xs-4" id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLAUg8wPXTabhE2uenCinJc-IkB607f5mzGdOKUpqZH-K1Gtlu"/>
+                <img className="card-img col-md-4 col-xs-4" id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlRo9f3QeZ2HfygAzbgbZHg4BfKNpre_eZuysD9jwXBotldV29"/>
+                <img className="card-img col-md-4 col-xs-4 pr-4" id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQlC0-VRfVSfArnhoHZGt8iCBE0ash5jqgSB5bhQiS-uCDWNfM"/>
+                <img className="card-img col-md-4 col-xs-4" id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTnv2-5xQrJBjGNOosj6znfqVa38iaBUVfTWa61xj8cT9h7CMhF"/>
+                <img className="card-img col-md-4 col-xs-4" id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKW5PWRfKi2NA6GkidyAqFMh9UVWRTOUZRO3FSXRqWd8lDduZF"/>
+                <img className="card-img col-md-4 col-xs-4 pr-4" id="imagen-home-starwars" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVquJ9ClQnn2xnTeIKV6GYfYEE5lZWvknSdBZihycMBx2Ifpma"/>
             </div>
             </div>
         </>

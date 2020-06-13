507a956321ef965b57b47c4b724d23d2e6797b68
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-13 14:08:05 -0400

    comp Character SW

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import FilmsStarWars from "./components/starwars/filmsStarWars";
 import PlanetsStarWars from "./components/starwars/planetsStarWars";
 import StarshipsStarWars from "./components/starwars/starshipsStarWars";
 import VehiclesStarWars from "./components/starwars/vehiclesStarWars";
+import SpeciesStarWars from "./components/starwars/speciesStarWars";
 
 const App = props => {
     return (
@@ -27,7 +28,13 @@ const App = props => {
                 <Route exact path="/projects/starwars/films" component={FilmsStarWars} />
                 <Route exact path="/projects/starwars/planets" component={PlanetsStarWars} />
                 <Route exact path="/projects/starwars/starships" component={StarshipsStarWars} />
+                <Route exact path="/projects/starwars/species" component={SpeciesStarWars} />
                 <Route exact path="/projects/starwars/vehicles" component={VehiclesStarWars} />
+                <Route exact path="/projects/starwars/people/character" component={} />
+                <Route exact path="/projects/starwars/planets/planet" component={VehiclesStarWars} />
+                <Route exact path="/projects/starwars/starships/starship" component={VehiclesStarWars} />
+                <Route exact path="/projects/starwars/species/specie" component={VehiclesStarWars} />
+                <Route exact path="/projects/starwars/vehicles/vehicle" component={VehiclesStarWars} />
 
             </Switch>
         </BrowserRouter>

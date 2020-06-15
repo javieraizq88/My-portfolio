2a3c422140db7c069e865fad22c39b580481a3ef
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-15 10:46:30 -0400

    CSS gato

--- src/components/tic-tac-toe/app.js
+++ src/components/tic-tac-toe/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '/style.css';
+import './App.css';
 import Board from './components/board';
 import Winner from './components/Winner';
 import Form from './components/Form';
--- src/components/tic-tac-toe/index.js
+++ src/components/tic-tac-toe/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TicTac from './App';
 import * as serviceWorker from './serviceWorker';
  // Add css files
  import 'bootstrap/dist/css/bootstrap.min.css';

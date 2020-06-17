379bb3ef2af2f58fbcf10dfac011355ffa5e3062
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-06-17 17:22:51 -0400

    component starships listo

--- src/components/starwars/starshipsStarWars.js
+++ src/components/starwars/starshipsStarWars.js
@@ -1,46 +1,73 @@
-import React, { useContext }  from "react";
+import React, { useContext } from "react";
 import { Context } from "../../store/appContext";
 import Titulo from "./tituloStarWars";
 import { Link } from "react-router-dom";
 
 const StarshipsStarWars = props => {
     const { store, actions } = useContext(Context);
-    const { people } = store;
 
     return (
         <>
-        <div id="container-home-starwars">
-        <Titulo/>
+            <div id="container-starwars">
+                <Titulo />
 
-        {/* Navbar */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top " id="navbar-starwars">
-            <Link className="navbar-brand" to="/projects/starwars">Star Wars</Link>
-                <div className="collapse navbar-collapse " id="navbarNav">
-                    <ul className="navbar-nav mr-auto border-right" id="navbar-item">
-                        <li className="nav-item">
-                            <Link className="nav-link border-left" to="/projects/starwars/people">People</Link>
-                        </li>
-                        <li className="nav-item ">
-                            <Link className="nav-link border-left" to="/projects/starwars/films">Films</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link className="nav-link border-left" to="/projects/starwars/starships">Starships</Link>
-                        </li>
-                        <li className="nav-item ">
-                            <Link className="nav-link border-left" to="/projects/starwars/vehicles">Vehicles</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link border-left" to="/projects/starwars/species">Species</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link border-left" to="/projects/starwars/planets">Planets</Link>
-                        </li>
-                    </ul>
-                    <Link type="button" className="btn btn-success" to="/projects" id="back-to-jim">Back to JIM</Link>
+                {/* Navbar */}
+                <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top " id="navbar-starwars">
+                    <Link className="navbar-brand" to="/projects/starwars">Star Wars</Link>
+                    <div className="collapse navbar-collapse " id="navbarNav">
+                        <ul className="navbar-nav mr-auto border-right" id="navbar-item">
+                            <li className="nav-item">
+                                <Link className="nav-link border-left" to="/projects/starwars/people">People</Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link className="nav-link border-left" to="/projects/starwars/films">Films</Link>
+                            </li>
+                            <li className="nav-item active">
+                                <Link className="nav-link border-left" to="/projects/starwars/starships">Starships</Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link className="nav-link border-left" to="/projects/starwars/vehicles">Vehicles</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link border-left" to="/projects/starwars/species">Species</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link border-left" to="/projects/starwars/planets">Planets</Link>
+                            </li>
+                        </ul>
+                        <Link type="button" className="btn btn-success" to="/projects" id="back-to-jim">Back to JIM</Link>
+                    </div>
+                </nav>
+
+                <div className="container" id="container-starships">
+                    <div className="row">
+                        <div className="row">
+                            {
+                                !!store.starships &&
+                                    store.starships.results.length > 0 ?
+                                    store.starships.results.map((starship, i) => {
+                                        const img = starship.name.split(" ").join("-").toLowerCase() + ".jpg";
+                                        return (
+                                            <div className="card mt-3 p-2 mx-3 ml-5 mr-5 mb-3 bg-dark" key={i} id="card-general-starships">
+                                                <div className="card-header ">
+                                                    <h3 className="card-title text-center text-white" id="titulo-starships">{starship.name}</h3>
+                                                </div>
+                                                <img src={"/img/Starships/" + img} className="card-img" alt="..." />
+                                                <p><Link to={"/starships/" + starship.name} className="mt-2 btn btn-danger ">More...</Link></p>
+                                            </div>
+                                        )
+                                    })
+                                    : (
+                                        <div className="col-md-12 col-xs-12 text-center">
+                                            <div className="spinner-grow" role="status">
+                                                <span className="sr-only">Loading...</span>
+                                            </div>
+                                        </div>
+                                    )
+                            }
+                        </div>
+                    </div>
                 </div>
-            </nav>
-
-
 
 
 

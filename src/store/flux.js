3729916fbc71852362758bbf13e3bde0d75ab7ed
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-07-06 18:33:18 -0400

    agregado fx en actions

--- src/store/flux.js
+++ src/store/flux.js
@@ -17,11 +17,36 @@ const getState = ({ getStore, getAction, setStore }) => {
 
         actions: {
 
-            // Form1
             handleChange: e => {
                 setStore({
                     [e.target.name]: e.target.value
                 })
+            },
+
+            // encuesta
+            cambiarCheckbox: (e) => {
+                const store = getStore()
+                if (store.marca.hasOwnProperty(e.target.name)) {
+                    const aux = { ...store.marca }
+                    delete aux[e.target.name]
+                    setStore({ marca: aux })
+                }
+                else {
+                    setStore({ marca: { ...store.marca, [e.target.name]: e.target.value } })
+                }
+            },
+            // le da un valor segun el atributo name y lo agrega al objeto vacío "marca"
+
+            cambiarRespuesta: (e) => {
+                const store = getStore()
+                if (store.respuesta.hasOwnProperty(e.target.name)) {
+                    const aux = { ...store.respuesta }
+                    delete aux[e.target.name]
+                    setStore({ respuesta: aux })
+                }
+                else {
+                    setStore({ respuesta: { ...store.respuesta, [e.target.name]: e.target.value } })
+                }
             },
 
             // Star Wars
@@ -126,7 +151,7 @@ const getState = ({ getStore, getAction, setStore }) => {
                         console.log(error)
                     })
             },
-        } 
+        }
     }
 }
 

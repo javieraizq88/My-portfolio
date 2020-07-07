f0d2c406081ec724426dcdafd838d22788fb6120
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-07-07 18:02:27 -0400

    finalizar encuesta

--- src/components/encuesta/finalizarEnc.js
+++ src/components/encuesta/finalizarEnc.js
@@ -1,8 +1,40 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Context } from "../../store/appContext"
+import HeaderEncuesta from "./headerEnc"
+import { Link } from "react-router-dom"
 
 const FinalizarEncuesta = props => {
+    const { store , actions } = useContext(Context)
+
+    console.log("nombre: " + store.name)
+    console.log("edad: " + store.age)
+    console.log(store.respuesta)
+
     return (
         <> 
+         <div >
+    <div className="card-group ">
+        <div className=" card-body col-md-1 col-xs-1"></div>
+        <div className="card-body col-md-10 col-xs-10"
+            id="container1">
+            <HeaderEncuesta />
+            <h2
+                id="subtitulo-finalizar">
+                Muchas gracias por completar la encuesta
+                </h2>
+                <Link
+                        id="boton-volver-home"
+                        to="/">
+                        <button
+                            type="button"
+                            className="btn btn-success">
+                            Completar otra encuesta
+                        </button>
+                    </Link>
+
+        </div>
+    </div>
+</div>
         </>
     )
 }

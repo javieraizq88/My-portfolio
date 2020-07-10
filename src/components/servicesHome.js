2625bb0f24a25d4f7dd46d78c074922659a723e8
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-07-10 18:48:03 -0400

    servicio actualizado

--- src/components/servicesHome.js
+++ src/components/servicesHome.js
@@ -16,39 +16,37 @@ const Services = props => {
                     <h1>Servicios</h1>
                     <hr />
 
-                    {/* Digital Postcard */}
-                    <div className="col-md-10 ml-5 mb-4">
-                        <div className="card h-100" id="card-InstagramPost">
-                            <h2 className="card-header" name="IP" id="titulo-card">Desarrollo de páginas web</h2>
-                            <div className="card-body">
-                                <h3 className="card-text-projects">Tecnologías y lenguajes</h3>
-                                <br />
-                                <h5 className="ml-5">
-                                    <strong> FRONT END:</strong> HTML 5 - CSS 3 - Bootstrap - JQuery - React.Js -JavaScript
+                    <br />
+                    <br />
+                    <br />
+                    <div className="card-group">
+                        <div className="col-md-1  mb-4"></div>
+                        <div className="col-md-10  mb-4">
+                            <div className="card h-100" id="card-servicios">
+                                <h2 className="card-header" name="IP" id="titulo-card">Desarrollo de páginas web</h2>
+                                <div className="card-body">
+                                    <h3 className="card-text-projects">Tecnologías y lenguajes</h3>
+                                    <br />
+                                    <h5 className="ml-5">
+                                        <strong> FRONT END:</strong> HTML 5 - CSS 3 - Bootstrap - JQuery - React.Js -JavaScript
                                 </h5>
-                                <br />
-                                <h5 className="ml-5">
-                                    <strong> BACK END:</strong> Python
+                                    <br />
+                                    <h5 className="ml-5">
+                                        <strong> BACK END:</strong> Python
                                 </h5>
 
-                                <br />
-                                <h5 className="ml-5">
-                                    <strong> BASE DE DATOS:</strong> SQLite
+                                    <br />
+                                    <h5 className="ml-5">
+                                        <strong> BASE DE DATOS:</strong> SQLite
                                 </h5>
+                                </div>
+
                             </div>
-
                         </div>
                     </div>
                     <br />
                     <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
                 </div>
-
             </div>
 
 

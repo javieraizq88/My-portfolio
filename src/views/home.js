948fdbcd6525a5c320729f48484bcfac0641d2f5
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-07-29 18:15:00 -0400

    cambio card

--- src/views/home.js
+++ src/views/home.js
@@ -156,6 +156,7 @@ const Home = props => {
                                     </div>
                                 </div>
                             </div>
+
                             <div class="col-md-4 mb-5">
                                 <div class="card h-100">
                                     <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
@@ -168,18 +169,8 @@ const Home = props => {
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4 mb-5">
-                                <div class="card h-100">
-                                    <img class="card-img-top" src="http://placehold.it/300x200" alt="" />
-                                    <div class="card-body">
-                                        <h4 class="card-title">Project 6</h4>
-                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <a href="#" class="btn btn-primary">Find Out More!</a>
-                                    </div>
-                                </div>
-                            </div>
+
+
                         </div>
                     </div>
 

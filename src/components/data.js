a315cdda894749998ee7db8d7383640f67a2b6c5
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-10-06 15:36:48 -0300

    imagen agregada del semaforo

--- src/components/data.js
+++ src/components/data.js
@@ -87,8 +87,8 @@ export default [
         "nombre": "Semáforo",
         "tecnology": "React.Js - JavaScript - CSS - Bootstrap - JQuery - HTML",
         "github": "https://github.com/javieraizq88/TrafficLightWithReact",
-        "pagina": "",
-        "imagen": ""
+        "pagina": "https://traffic-light-with-react.now.sh",
+        "imagen": "/img/1/traffic-light.jpg"
     },
     {
         "nombre": "Formulario",
